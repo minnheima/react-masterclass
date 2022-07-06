fbb1f9818f56767c7e8bc136b159c68f13c8b5e9
minnheima <hertz.minn@gmail.com>
2022-07-06 17:52:03 +0900

    #3.3 Optional Props

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
 
 const darkTheme = {
@@ -15,8 +14,6 @@ const lightTheme = {
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
