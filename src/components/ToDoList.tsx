2db957d54f03402a56e6dc11f69c1b0f0d15466c
minnheima <hertz.minn@gmail.com>
2022-07-20 16:34:41 +0900

    #5.12 Refactoring

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,47 +1,19 @@
-import { useForm } from "react-hook-form";
-import { atom, useRecoilState } from "recoil";
-
-interface IForm {
-  toDo: string;
-}
-interface IToDo {
-  text: string;
-  id: number;
-  category: "TO_DO" | "DOING" | "DONE"; //모든 string이 아니라 이 셋 중에 하나
-}
-
-// TypeScript는 toDos가 여기의 IToDo 객체로 이뤄진 배열임을 안다
-const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-});
+import { useRecoilValue } from "recoil";
+import { toDoState } from "../atom";
+import CreateToDo from "./CreatToDo";
+import ToDo from "./ToDo";
 
 function ToDoList() {
-  const [toDos, setToDos] = useRecoilState(toDoState); //toDos와 setter함수 모두 리턴
+  const toDos = useRecoilValue(toDoState); // setter함수가 필요하지 않기때문에 useRecoilValue로 변경
 
-  const { register, handleSubmit, setValue } = useForm<IForm>();
-  const handleValid = ({ toDo }: IForm) => {
-    // console.log("add to do", data.toDo);
-    setToDos((oldToDos) => [{ text: toDo, id: Date.now(), category: "TO_DO" }, ...oldToDos]); // []에 oldToDo의 요소들을 넣어 반환
-    setValue("toDo", "");
-  };
-  console.log(toDos);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
-      <form onSubmit={handleSubmit(handleValid)}>
-        <input
-          {...register("toDo", {
-            required: "Please write a To Do",
-          })}
-          placeholder="Write a to do"
-        />
-      </form>
-      <button>Add</button>
+      <CreateToDo />
       <ul>
         {toDos.map((toDo) => (
-          <li key={toDo.id}>{toDo.text}</li>
+          <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
     </div>
