e181e13e118437337d7eef6851a373fc19734d69
minnheima <hertz.minn@gmail.com>
2022-07-26 15:46:49 +0900

    #6.0 Get Selectors (reset components)

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,20 @@
-import { createGlobalStyle } from "styled-components";
-import ToDoList from "./components/ToDoList";
-
-const GlobalStyle = createGlobalStyle`
-  html, body, div, span, applet, object, iframe,
-  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-  a, abbr, acronym, address, big, cite, code,
-  del, dfn, em, img, ins, kbd, q, s, samp,
-  small, strike, strong, sub, sup, tt, var,
-  b, u, i, center,
-  dl, dt, dd, menu, ol, ul, li,
-  fieldset, form, label, legend,
-  table, caption, tbody, tfoot, thead, tr, th, td,
-  article, aside, canvas, details, embed,
-  figure, figcaption, footer, header, hgroup,
-  main, menu, nav, output, ruby, section, summary,
-  time, mark, audio, video {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    font: inherit;
-    vertical-align: baseline;
-  }
-  /* HTML5 display-role reset for older browsers */
-  article, aside, details, figcaption, figure,
-  footer, header, hgroup, main, menu, nav, section {
-    display: block;
-  }
-  /* HTML5 hidden-attribute fix for newer browsers */
-  *[hidden] {
-    display: none;
-  }
-  *{
-    box-sizing: border-box;
-  }
-  body {
-    font-family: 'Noto Sans KR', sans-serif;
-    font-weight: 300;
-    line-height: 1.2;
-    background-color: ${(props) => props.theme.bgColor};
-    color: ${(props) => props.theme.textColor};
-  }
-  menu, ol, ul, li {
-    list-style: none;
-  }
-  blockquote, q {
-    quotes: none;
-  }
-  blockquote:before, blockquote:after,
-  q:before, q:after {
-    content: '';
-    content: none;
-  }
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-  button {
-    border:none;
-  }
-`;
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atom";
 
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
+
+  const onMinutesChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+e.currentTarget.value);
+    // + if you add plus(+), string value is converted to number / ex) "1"->1
+  };
+
   return (
     <>
-      <GlobalStyle />
-      <ToDoList />
+      <input value={minutes} onChange={onMinutesChange} type="number" placeholder="minutes" />
+      <input value={hours} type="number" placeholder="hours" />
     </>
   );
 }
--- src/atom.tsx
+++ src/atom.tsx
@@ -1,36 +1,14 @@
 import { atom, selector } from "recoil";
 
-// type categories = "TO_DO" | "DOING" | "DONE"; //재사용가능
-// 실수를 줄이기 위해서 이렇게 값을 넣어 사용하기
-// string으로 바꾸고 싶다면  "TO_DO"="TO_DO", 이런식으로도 가능하다
-export enum Categories {
-  "TO_DO" = "TO_DO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
-}
-export interface IToDo {
-  text: string;
-  id: number;
-  category: Categories;
-}
-export const TODO_LIST = "todos";
-export const categoryState = atom<Categories>({
-  key: "category",
-  default: Categories.TO_DO,
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
 });
 
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-});
-
-// Selector 는 atom을 가져다가 output을 변형하는 것. state 자체를 바꾸는 게 아니다
-// Selector 로 category별로 분류해줌
-export const toDoSelector = selector({
-  key: "toDoSelector",
+export const hourSelector = selector({
+  key: "hours",
   get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
+    const minutes = get(minuteState);
+    return minutes / 60;
   },
 });
