0b3672b03ac6a23c8b7afe268be081a267617320
minnheima <hertz.minn@gmail.com>
2022-07-26 16:22:13 +0900

    #6.1 Set Selectors

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { hourSelector, minuteState } from "./atom";
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
+  const [hours, setHours] = useRecoilState(hourSelector);
 
   const onMinutesChange = (e: React.FormEvent<HTMLInputElement>) => {
     setMinutes(+e.currentTarget.value);
     // + if you add plus(+), string value is converted to number / ex) "1"->1
   };
+  const onHoursChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setHours(+e.currentTarget.value);
+  };
 
   return (
     <>
-      <input value={minutes} onChange={onMinutesChange} type="number" placeholder="minutes" />
-      <input value={hours} type="number" placeholder="hours" />
+      <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes" />
+      <input value={hours} onChange={onHoursChange} type="number" placeholder="Hours" />
     </>
   );
 }
